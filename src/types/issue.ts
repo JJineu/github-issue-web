a814b26ba936cc72852a53b331ec724b6c7fda17
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-29 19:54:13 +0900

    FEAT: type 설정 및 더미데이터 저장

---
+++ src/types/issue.ts
@@ -0,0 +1,17 @@
+export interface IIssue {
+  issueId: number; // number
+  title: string;
+  author: string;
+  createAt: string;
+  commentsLength: number; // 2
+  state: string; // state - closed, 1 open
+}
+export interface IIssueDetail {
+  issueId: number; // number
+  title: string;
+  author: string; // user.login
+  createdAt: string; // created_at
+  commentsLength: number; // comments
+  profileImage: string; // user.avatar_url
+  body: string; // markdown
+}
