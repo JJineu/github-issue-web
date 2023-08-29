1914023a075a727698d5d4f14ef4164329b2e72c
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-29 21:29:15 +0900

    FEAT: 공통 헤더 구현

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { OWNER, REPO } from '../constants';
 
 export default function Layout() {
   return (
     <>
-      {/* {Organization Name / Repository Name} */}
+      <h1>
+        {OWNER} / {REPO}
+      </h1>
       <Outlet />
     </>
   );
