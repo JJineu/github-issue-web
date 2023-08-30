1ef3889ed07660edd59e0d044ff91020c69e410d
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-30 15:12:55 +0900

    FEAT: useIntersect 훅 구현

---
+++ src/hooks/useIntersect.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+type IntersectHandler = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
+
+const useIntersect = (onIntersect: IntersectHandler, options?: IntersectionObserverInit) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const checkIntersect = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          onIntersect(entry, observer); // callback
+          observer.observe(entry.target);
+        }
+      });
+    },
+    [onIntersect],
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+    // ref 렌더링 이후 oberve
+    const observer = new IntersectionObserver(checkIntersect, options);
+    observer.observe(ref.current);
+
+    // 언마운트시 클린업
+    return () => observer.disconnect();
+  }, [ref, options, checkIntersect]);
+
+  return ref;
+};
+
+export default useIntersect;
