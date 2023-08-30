e9dc49397256cc8bd2738c996bb84855298525a2
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-30 12:39:29 +0900

    REFACTOR: issueCard 재사용을 위해 리팩토링

--- src/components/issue/IssueCard.tsx
+++ src/components/issue/IssueCard.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { IIssue } from '../../types/issue';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function IssueCard({ issue }: { issue: IIssue }) {
   const { issueId, title, author, createdAt, commentsLength } = issue;
   const navigate = useNavigate();
+  const location = useLocation();
   const moveIssueDetail = () => {
-    navigate(`/${issueId}`);
+    if (location.pathname !== `/${issueId}`) navigate(`/${issueId}`);
   };
 
   return (
-    <section onClick={moveIssueDetail}>
-      <p>{issueId}</p>
-      <title>{title}</title>
-      <p>{author}</p>
-      <p>{createdAt}</p>
+    <Container onClick={moveIssueDetail}>
+      <p>#{issueId}</p>
+      <div>
+        <title>{title}</title>
+        <p>
+          작성자: {author}, 작성일: {createdAt}
+        </p>
+      </div>
       <p>{commentsLength}</p>
-    </section>
+    </Container>
   );
 }
+
+const Container = styled.section`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+`;
