abf385d2b2542c07dd60c034e6c4023a09bbe9c4
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-30 12:32:52 +0900

    STYLE: 전체 레이아웃 및 스타일 일부 적용

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { OWNER, REPO } from '../constants';
+import styled from 'styled-components';
 
 export default function Layout() {
   return (
-    <>
-      <h1>
-        {OWNER} / {REPO}
-      </h1>
+    <Container>
+      <Header>
+        <h1>
+          {OWNER} / {REPO}
+        </h1>
+      </Header>
       <Outlet />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.main`
+  display: flex-col;
+  padding: 10px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px 0;
+`;
