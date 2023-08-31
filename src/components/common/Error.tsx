2eae43c97585d186df6ff3bbca2c2e8793c3205a
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-08-31 16:04:52 +0900

    STYLE: error page 스타일링

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Error() {
-  return <div></div>;
+  return (
+    <DivContainer>
+      <p>Error</p>
+      <p>예상치 못한 오류가 발생했습니다.</p>
+    </DivContainer>
+  );
 }
+
+const DivContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  min-width: 480px;
+  font-size: 50px;
+  font-weight: bold;
+  > :last-child {
+    font-size: 30px;
+  }
+`;
