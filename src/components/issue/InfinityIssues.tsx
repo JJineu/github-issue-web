ff0aa6f3462ddb5da300ffcbdc5c60849faac0fd
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-09-01 19:23:08 +0900

    REFACTOR: 이슈리스트와 무한스크롤 로직 분리

--- src/components/issue/InfinityIssues.tsx
+++ src/components/issue/InfinityIssues.tsx
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
 import Loading from '../common/Loading';
 import useIntersect from '../../hooks/useIntersect';
 import { useIssuesContext } from '../../context/IssuesProvider';
-import Error from '../common/Error';
+import useFetch from '../../hooks/useFetch';
+import { IIssue } from '../../types/issue';
+import { getIssues } from '../../api/request';
 
-const InfinityIssues = () => {
-  const { addIssues, page, callback, loading, error } = useIssuesContext();
+export default function InfinityIssues() {
+  const { page, setPage, addIssues } = useIssuesContext();
+  const { data, loading, error, fetch } = useFetch<IIssue[], Error>(getIssues);
 
-  const getNextPage = async () => {
+  const getNextPage = () => {
     if (!loading && !error) {
-      page.current = page.current + 1;
-      const nextIssues = await callback();
-      addIssues(nextIssues);
+      setPage((prev) => (prev += 1));
     }
   };
-
   const targetRef = useIntersect(getNextPage);
-  return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : error ? (
-        <Error />
-      ) : (
-        <TargetDiv ref={targetRef}>
-          <Loading />
-        </TargetDiv>
-      )}
-    </>
-  );
-};
 
-export default InfinityIssues;
+  useEffect(() => {
+    fetch(() => getIssues(page));
+  }, [page]);
+
+  useEffect(() => {
+    if (data) {
+      addIssues(data);
+    }
+  }, [data]);
+
+  return (
+    <TargetDiv ref={targetRef}>
+      <Loading />
+    </TargetDiv>
+  );
+}
 
 const TargetDiv = styled.div`
   width: 100%;
--- src/pages/issue/Issue.tsx
+++ src/pages/issue/Issue.tsx
@@ -1,42 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import IssueCard from '../../components/issue/IssueCard';
 import { IIssue } from '../../types/issue';
 import AdCard from '../../components/ad/AdCard';
-import Loading from '../../components/common/Loading';
-import Error from '../../components/common/Error';
 import { useIssuesContext } from '../../context/IssuesProvider';
-// import InfinityIssues from '../../components/issue/InfinityIssues';
-import useFetch from '../../hooks/useFetch';
-import { getIssues } from '../../api/request';
-import styled from 'styled-components';
-import useIntersect from '../../hooks/useIntersect';
+import InfinityIssues from '../../components/issue/InfinityIssues';
 
-const Issue = () => {
-  const { issues, setIssues } = useIssuesContext();
-  const [page, setPage] = useState(1);
-  const { data, loading, error, refetch } = useFetch<IIssue[], Error>();
+export default function Issue() {
+  const { issues } = useIssuesContext();
 
-  const getNextPage = () => {
-    if (!loading && !error) {
-      setPage((prev) => prev + 1);
-    }
-  };
-
-  useEffect(() => {
-    refetch(() => getIssues(page));
-  }, [page]);
-
-  useEffect(() => {
-    if (data) {
-      setIssues([...issues, ...data]);
-    }
-  }, [data]);
-
-  const targetRef = useIntersect(getNextPage);
   return (
     <>
-      {loading ?? <Loading />}
-      {error && <Error />}
       {issues &&
         issues.map((issue: IIssue, index: number) => (
           <React.Fragment key={issue.issueId}>
@@ -44,17 +17,7 @@ const Issue = () => {
             {(index + 1) % 4 === 0 && <AdCard />}
           </React.Fragment>
         ))}
-      <TargetDiv ref={targetRef}>
-        <Loading />
-      </TargetDiv>
-      {/* <InfinityIssues /> */}
+      <InfinityIssues />
     </>
   );
-};
-
-export default Issue;
-
-const TargetDiv = styled.div`
-  width: 100%;
-  height: 1px;
-`;
+}
